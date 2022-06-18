fc39e838199df6022667a8b8ddc825ea014a6fec
toMilanesi <tomi.milanesi@gmail.com>
2022-06-18 10:48:24 -0300

    Added modules and test

---
+++ modules/calculator.js
@@ -0,0 +1,29 @@
+const add = (num1, num2) => num1 + num2;
+
+const substract = (num1, num2) => num1 - num2;
+
+const multiply = (num1, num2) => {
+  const result = num1 * num2;
+  if(Number.isInteger(result)) {
+    return result;
+  } else {
+    return parseInt(result.toFixed(2));
+  }
+};
+
+const divide = (num1, num2) => {
+  if (num2 == 0) {
+    return NaN;
+  } else if (num1 == 0) {
+    return 0;
+  } else {
+    const result = num1 / num2;
+    if (Number.isInteger(result)) {
+      return result;
+    } else {
+      return parseFloat(result.toFixed(2));
+    }
+  }
+}
+
+module.exports = { add, substract, multiply, divide };
