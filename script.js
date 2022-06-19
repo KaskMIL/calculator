a3cab5540975f57f1946f58e9678863173f45228
toMilanesi <tomi.milanesi@gmail.com>
2022-06-19 12:32:47 -0300

    add num data

--- script.js
+++ script.js
@@ -13,6 +13,20 @@ const optBtn =document.querySelectorAll('.button-options');
 const operBtn = document.querySelectorAll('.button-operator');
 const numBtn =document.querySelectorAll('.button-number');
 
+let dataCont = [];
+const numData = [
+    {id: 'zero', num: 0},
+    {id: 'one', num: 1},
+    {id: 'two', num: 2},
+    {id: 'three', num: 3},
+    {id: 'four', num: 4},
+    {id: 'five', num: 5},
+    {id: 'six', num: 6},
+    {id: 'seven', num: 7},
+    {id: 'eigth', num: 8},
+    {id: 'nine', num: 9}
+];
+
 btnContainer.addEventListener('click', (e) => {
     numBtn.forEach(element => {
         if (e.target.innerHTML === element.innerHTML) {
